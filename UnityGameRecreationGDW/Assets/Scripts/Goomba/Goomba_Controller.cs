33a39f72a018c7c3bf1ed470f06a29cb85ffe553
Ethan Chiasson <ethan.chiasson@gmail.com>
2025-11-24 17:11:25 -0500

    Building out level + Mario and camera functionality
    Added Lots of art assets and built out the base level for the game, added functionality for mario, goomba and camera for better orientation

--- UnityGameRecreationGDW/Assets/Scripts/Goomba/Goomba_Controller.cs
+++ UnityGameRecreationGDW/Assets/Scripts/Goomba/Goomba_Controller.cs
@@ -4,6 +4,13 @@ public class Goomba_Controller : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Transform yr;
+    public GameObject Sprite;
+    private Transform spriteflipper;
+
+
+    public float Spinspeed;
+    private float MaxSpinspeed;
+    public float StepCount;
     public float speed;
     public int direction = 1; // to flip direction of goomba when colliding with wall
 
@@ -11,11 +18,20 @@ public class Goomba_Controller : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         yr = GetComponent<Transform>();
+        spriteflipper = Sprite.GetComponent<Transform>();
+        MaxSpinspeed = Spinspeed;
     }
 
     void Update()
     {
-        
+        Spinspeed -= Time.deltaTime;
+        if (Spinspeed <= 0)
+        {
+            StepCount += 1;
+            Spinspeed = MaxSpinspeed; 
+            GoombaRotate();
+        }
+
     }
 
     private void FixedUpdate()
@@ -29,6 +45,17 @@ public class Goomba_Controller : MonoBehaviour
         {
             direction *= -1; // Flip direction
             yr.Rotate(0f, 180f, 0f);
+        }
+    }
+    private void GoombaRotate()
+    {
+        if (StepCount % 2 == 0)
+        {
+            spriteflipper.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else
+        {
+            spriteflipper.rotation = Quaternion.Euler(0, 180, 0);
         }
     }
 }
